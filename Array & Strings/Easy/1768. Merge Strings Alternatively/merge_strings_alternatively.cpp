fadcc7727f5408396982548f7c3c29ab285265bf
Gitika Goyal <gitikagoyal1115@gmail.com>
2025-06-18 19:29:09 +0530

    Create merge_strings_alternatively.cpp

---
+++ Array & Strings/Easy/1768. Merge Strings Alternatively/merge_strings_alternatively.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string result = "";
+        int i= 0;
+        int j=0;
+        while (i < word1.length() && j < word2.length()) {
+            result += word1[i];
+            result += word2[j];
+            i++;
+            j++;
+        }
+        result += word1.substr(i);
+        result += word2.substr(j);
+        return result;
+    }
+};
