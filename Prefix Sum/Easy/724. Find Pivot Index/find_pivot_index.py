0b2612ee192befe90d7118c5da2b6904892dbad5
Gitika Goyal <gitikagoyal1115@gmail.com>
2025-06-21 23:47:43 +0530

    Update find_pivot_index.py

--- Prefix Sum/Easy/724. Find Pivot Index/find_pivot_index.py
+++ Prefix Sum/Easy/724. Find Pivot Index/find_pivot_index.py
@@ -39,3 +39,23 @@ class Solution:
 # Prefix Suffix Sum Solution
 # T.C.->O(3n)=>O(n)
 # S.C.->O(2n)=>O(n)
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        n = len(nums)
+        prefix = [0] * n
+        prefix[0] = nums[0]
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] + nums[i]
+        
+        suffix = [0] * n
+        suffix[n - 1] = nums[n - 1]
+        for i in range(n - 2, -1, -1):
+            suffix[i] = suffix[i + 1] + nums[i]
+        
+        for i in range(n):
+            pref = prefix[i - 1] if i > 0 else 0
+            suf = suffix[i + 1] if i < n - 1 else 0
+            if pref == suf:
+                return i
+        
+        return -1
