7f6f276fbfa429520e174526e82f8a4b5a3d05bb
Gitika Goyal <gitikagoyal1115@gmail.com>
2025-06-26 22:06:26 +0530

    Create number_of_recent_calls.py

---
+++ Queue/Easy/933. Number of Recent Calls/number_of_recent_calls.py
@@ -0,0 +1,38 @@
+#Approach-I (Brute-Force using Linear Scan)
+#T.C.-> O(n)
+#S.C.-> O(total no. of pings)
+class RecentCounter:
+    def __init__(self):
+        self.q = list()
+    def ping(self, t: int) -> int:
+        if t is None:
+            return None
+        else:
+            self.q.append(t)
+            srange = t-3000
+            erange = t
+            count = 0
+            for i in self.q:
+                if i>=srange and i<=erange:
+                    count+=1
+            return count
+        
+#Approach-II (Using Queue)
+#T.C.-> O(1)
+#S.C.-> O(total no. of pings)
+from collections import deque
+class RecentCounter:
+
+    def __init__(self):
+        self.q = deque()
+        
+
+    def ping(self, t: int) -> int:
+        self.q.append(t)
+        srange = t-3000
+        erange = t
+        count = 0
+        while self.q[0]<srange:
+            self.q.popleft()
+        return len(self.q)
+    
