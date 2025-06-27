8b1f0fd4b1d783afa368956b6358fbe4ed7a9aa0
Gitika Goyal <gitikagoyal1115@gmail.com>
2025-06-27 18:01:15 +0530

    Create reverse_linked_list.cpp

---
+++ Linked List/Easy/206. Reverse Linked List/reverse_linked_list.cpp
@@ -0,0 +1,61 @@
+//Approach-Recursion
+//T.C : O(n)
+//S.C : O(n) 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head) {
+            return nullptr;
+        }
+
+        ListNode* newHead = head;
+        if (head->next) {
+            newHead = reverseList(head->next);
+            head->next->next = head;
+        }
+        head->next = nullptr;
+
+        return newHead;
+    }
+};
+
+
+//Approach-Iterative
+//T.C : O(n)
+//S.C : O(1)
+                              
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        while (curr) {
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+};
