2718056b4879dbd821bde7c9b2e474925227290c
Gitika Goyal <gitikagoyal1115@gmail.com>
2025-06-30 08:12:33 +0530

    Create maximum_depth_of_binaary_tree.py

---
+++ Binary Tree - DFS/Easy/104. Maximum Depth of Binary Tree/maximum_depth_of_binaary_tree.py
@@ -0,0 +1,46 @@
+#Approach-1 (Recursive DFS)
+#T.C.-> O(n)
+#S.C.-> O(h)
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+#Approach-2 (Iterative-DFS)
+#T.C.->O(n)
+#S.C.->O(n)
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return res
+
+
+#Approach-3 (BFS)
+#T.C.->O(n)
+#S.C.->O(n)
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        q = deque()
+        if root:
+            q.append(root)
+
+        level = 0
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
